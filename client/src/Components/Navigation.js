54197dfee5432765db90a08b640b1b86d5b83bb6
Becca Yu <ugsung8970@gmail.com>
2022-07-26 15:54:49 -0700

    commit 2 setting up framework for react app

---
+++ client/src/Components/Navigation.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+function Navigation() {
+    return(
+        <nav className='nav navbar'>
+            <ul>
+
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
