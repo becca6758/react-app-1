2869fb0fc363484bd70bfddd69abb1f18ddd1340
Becca Yu <ugsung8970@gmail.com>
2022-08-04 16:49:40 -0700

    setting up client src application

---
+++ client/src/Pages/category.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+export default category;
