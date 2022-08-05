4c5b9ec682a87ecb4ca6c30a0e6529939d578885
Becca Yu <ugsung8970@gmail.com>
2022-08-05 16:42:21 -0700

    setting up components and removed pages folder

---
+++ client/src/Components/Home.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+//import { Link, useNavigate } from "react-router-dom";
+
+function Home({ details }) {
+   
+    const [searchField, setSearchField] = useState("");
+}
+
+export default Home;
--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,3 +1,9 @@
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Navigation } from "./Navigation";
+export { default as Home } from "./Home";
+export { default as Category } from "./Category";
+export { default as Brand } from "./Brand";
+export { default as ProductCard } from "./ProductCard";
+export { default as Scroll } from "./Scroll";
+export { default as SearchList } from "./SearchList";
